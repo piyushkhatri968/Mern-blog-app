212c11c53aefd9b79d2a5d92998f9a5f20b95d4d
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-19 03:38:13 +0500

    updated

--- client/src/Components/PostCard.jsx
+++ client/src/Components/PostCard.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="group mt-5 relative border border-teal-500 hover:border-2 h-[300px] overflow-hidden rounded-lg w-full sm:w-[350px] transition-all">
+    <div className="group relative border border-teal-500 hover:border-2 h-[370px] overflow-hidden rounded-lg w-full sm:w-[350px] transition-all">
       <Link to={`/post/${post.slug}`}>
         <img
-          src={post && post.image}
-          alt="article cover"
+          src={post.image}
+          alt="post cover"
           className="h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20"
         />
       </Link>
-      <div className="p-3 flex flex-col gap-2 ">
+      <div className="p-3 flex flex-col gap-2">
         <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
         <span className="italic text-sm">{post.category}</span>
         <Link
