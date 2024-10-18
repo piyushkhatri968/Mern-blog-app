be99d40aaa486c30ede051c3180d15942177c070
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-19 01:17:25 +0500

    added

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,7 +1,63 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import CallToAction from "../Components/CallToAction";
+import PostCard from "../Components/PostCard";
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchPosts = async () => {
+        const res = await fetch(`/api/post/getposts`);
+        if (res.ok) {
+          const data = await res.json();
+          setPosts(data.posts);
+        }
+      };
+      fetchPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome To My Blog</h1>
+        <p className="text-gray-500 text-sm sm:text-md">
+          Here you'll find a variety of articles and tutorials on topics such as
+          web developement, software engineering, and programming languages
+        </p>
+        <Link
+          to={`/search`}
+          className="text-xs sm:text-sm text-teal-500 hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
