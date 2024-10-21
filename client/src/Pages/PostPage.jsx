b02b5390e8bbc1616864a715b57bcb71622656ce
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-21 13:16:13 +0500

    update

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -67,7 +67,7 @@ const PostPage = () => {
 
   return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-      {currentUser && <Button className="text-right">Edit</Button>}
+      <Button className="text-right max-w-10">Edit</Button>
       <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
       </h1>
