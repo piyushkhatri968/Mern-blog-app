96c78499a48ae6c7d89c1be21ff0abc2cffe4515
Piyush Khatri <piyushkhatri968@gmail.com>
2024-11-22 18:11:17 +0500

    a

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -16,11 +16,12 @@ const Dashboard = () => {
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
+    
   }, [location.search]);
 
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md:w-56">
+      <div className="md:w-64">
         {/* SideBar */}
         <DashSidebar />
       </div>
